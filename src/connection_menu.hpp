1db69c71e33053ea6cba5ff824a8dc1971ca0221
Thomas Gollenia <thomas@gollenia.at>
2025-11-02 21:26:35 +0100

    removed shortcut

--- src/connection_menu.hpp
+++ src/connection_menu.hpp
@@ -12,10 +12,10 @@ Component ConnectionMenu(std::vector<std::string>* entries, int* selected) {
 	auto option = MenuOption::Vertical();
 	option.entries.transform = [](EntryState state) {
 		std::vector<std::string> labelArray = StringHelpers::split(state.label, '|');
-		std::string numberColumn = (std::stoi(labelArray[0]) < 10 ? labelArray[0] + "  " : labelArray[0] + " ");
+		std::string numberColumn = (std::stoi(labelArray[0]) < 10 ?  "  " + labelArray[0] : " " + labelArray[0] );
 		Element e = hbox({
-			text(" " + numberColumn + " " + labelArray[1] + "   " + labelArray[2]) | flex,
-			text(labelArray[3] + " "),
+			text(" " + numberColumn + " " + labelArray[1]) | flex,
+			text(labelArray[2] + " "),
 		});
 		if (state.focused)
 		e = e | bgcolor(Color::Blue);
